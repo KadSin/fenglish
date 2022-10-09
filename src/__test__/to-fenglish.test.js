cf5845436af4196bb9b27de24cd6a781830e70f7
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2022-10-09 16:45:15 +0330

    Return peer to peer fenglish letters

--- src/__test__/to-fenglish.test.js
+++ src/__test__/to-fenglish.test.js
@@ -1,7 +1,10 @@
+import alefTestCases from './test-cases/alef'
 import { toFenglish } from '../to-fenglish'
 
 describe('toFenglish', () => {
 	it('Should return exact testcases for letter `alef`', () => {
-		toFenglish()
+		for(let persian in alefTestCases)
+			expect(toFenglish(persian))
+				.toEqual(alefTestCases[persian])
 	})
 })
--- src/to-fenglish.js
+++ src/to-fenglish.js
@@ -1,5 +1,13 @@
-import fenglishMap from './letters-map'
+import lettersMap from './letters-map'
 
-export function toFenglish() {
-	console.log(fenglishMap)
+export function toFenglish(text) {
+	let fenglish = []
+
+	for(let i = 0;i < text.length;i++) {
+		let current = text[i]
+
+		fenglish.push(lettersMap[current] || current)
+	}
+
+	return fenglish.join('')
 }
