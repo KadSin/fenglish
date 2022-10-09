5ccb6b67e4db9f857ee8696a9b5c325a80708347
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2022-10-09 16:46:23 +0330

    Correct the alef according to vowels in `toFenglish`

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const ALEF = 'ا'
+
+/** @constant VOWELS an en on a e o */
+const VOWELS = ['ً', 'ٍ', 'ٌ', 'َ', 'ِ', 'ُ']
+
+export { ALEF, VOWELS }
--- src/to-fenglish.js
+++ src/to-fenglish.js
@@ -1,10 +1,17 @@
 import lettersMap from './letters-map'
+import { ALEF, VOWELS } from './constants'
 
 export function toFenglish(text) {
 	let fenglish = []
 
 	for(let i = 0;i < text.length;i++) {
 		let current = text[i]
+		const next = text[i + 1]
+
+		if(VOWELS.includes(next) && current == ALEF) {
+			current = next
+			i++
+		}
 
 		fenglish.push(lettersMap[current] || current)
 	}
