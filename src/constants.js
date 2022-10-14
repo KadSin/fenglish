7173538f9d9bda16a6ad351a8fd18841cf82c2e5
sina <kadkhodadeisina@gmail.com>
2022-10-14 23:05:26 +0330

    Support all `alef` types in toFenglish

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
+const A_BA_KOLAH = 'آ'
 const ALEF = 'ا'
 
 /** @constant VOWELS an en on a e o */
 const VOWELS = ['ً', 'ٍ', 'ٌ', 'َ', 'ِ', 'ُ']
 
-export { ALEF, VOWELS }
+export { A_BA_KOLAH, ALEF, VOWELS }
