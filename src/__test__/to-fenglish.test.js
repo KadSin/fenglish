8e5151f64d73a98b33adaf13f5ce26318e160d63
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2022-10-15 15:23:46 +0330

    Refactor the `toFenglish` tests for alef

--- src/__test__/to-fenglish.test.js
+++ src/__test__/to-fenglish.test.js
@@ -1,10 +1,41 @@
-import alefTestCases from './test-cases/alef'
 import { toFenglish } from '../to-fenglish'
 
-describe('toFenglish', () => {
-	it('Should return exact testcases for letter `alef`', () => {
-		for(let persian in alefTestCases)
-			expect(toFenglish(persian))
-				.toEqual(alefTestCases[persian])
+/**
+ * @description `X` point to `Consonant` and `Y` point to `Vowel`
+ */
+
+describe('toFenglish for ALEF', () => {
+	it('Should overwrite the alef by next vowel', () => {
+		expectPersianIsFenglish(
+			['اَشکال', 'اَبرها', 'اِشتِها', 'اِجرا', 'اَسب'],
+			['ashkal', 'abrha', 'eshteha', 'ejra', 'asb'],
+		)
+	})
+
+	it('Word with <A_BA_KOLAH + Consonant + vowel> format should be `aXY`', () => {
+		expectPersianIsFenglish(
+			['آذَر', 'آدَم', 'آرَش', 'آسِمان', 'آشِکار'],
+			['azar', 'adam', 'arash', 'aseman', 'ashekar'],
+		)
+	})
+
+	it('Word with <Consonant + ALEF vowel + Consonant> format should be `XaaX`', () => {
+		expectPersianIsFenglish(
+			['خام', 'بام', 'فال', 'طاق', 'راز', 'لات'],
+			['khaam', 'baam', 'faal', 'taagh', 'raaz', 'laat'],
+		)
+	})
+
+	it('Word with <A_BA_KOLAH + Consonant + ALEF vowel(long vowel)> format should be `aXaa`', () => {
+		expectPersianIsFenglish(
+			['آثار', 'آزار', 'آرام', 'آباد', 'آداب'],
+			['asaar', 'azaar', 'araam', 'abaad', 'adaab'],
+		)
 	})
 })
+
+function expectPersianIsFenglish(persian, fenglish) {
+	persian.forEach(
+		(word, i) => expect(toFenglish(word)).toEqual(fenglish[i]),
+	)
+}
--- src/to-fenglish.js
+++ src/to-fenglish.js
@@ -4,66 +4,37 @@ import { A_BA_KOLAH, ALEF, VOWELS } from './constants'
 export function toFenglish(text) {
 	let fenglish = ''
 
-	const first = text[0]
-
 	for(let i = 0;i < text.length;i++) {
 		const before = text[i - 1]
 		let current = text[i]
 		const next = text[i + 1]
 
 		if(VOWELS.includes(next)) {
-			/**
-			 * @example abrha
-			 * @description Replace the alef with next vowel
-			 */
 			if(current == ALEF) {
 				current = next
 				i++
 			}
 
-			/**
-			 * @example azar
-			 * @description The initial A_BA_KOLAH should be one `a` when format is <A_BA_KOLAH + Consonant + vowel>
-			 */
 			if(before == A_BA_KOLAH) {
 				fenglish = fenglish.slice(0, -1)
 			}
-		} else {
-			if(i == 1) {
-				/**
-				 * @example khaam
-				 * @description Add additional `a` to the words with three letters with <Consonant + ALEF vowel + Consonant> format
-				 */
-				if(current == ALEF && text.length == 3) {
-					fenglish = fenglish + 'a'
-				}
+		}else{
+			if(current == ALEF && text.length == 3) {
+				fenglish = fenglish + 'a'
+			}
 
-				if(first == A_BA_KOLAH && next != undefined) {
-					if(hasAlef(next)) {
-						/**
-						 * @example asaar
-						 * @description The initial A_BA_KOLAH should be one `a` and second alef should be two `a` when format is
-						 * <A_BA_KOLAH + Consonant + ALEF vowel(long vowel)>
-						 */
-						fenglish = fenglish.slice(0, -1) + (lettersMap[current] || current) + 'a'
-						continue
-					} else {
-						/**
-						 * @example astara
-						 * @description The initial A_BA_KOLAH should be one `a` when format is <A_BA_KOLAH + Consonant + Consonant>
-						 */
-						fenglish = fenglish.slice(0, -1)
-					}
-				}
+			if(i == 1 && before == A_BA_KOLAH && next == ALEF) {
+				fenglish = 'a' + fenglishLetter(current) + 'a'
+				continue
 			}
 		}
 
-		fenglish = fenglish + lettersMap[current] || current
+		fenglish = fenglish + fenglishLetter(current)
 	}
 
 	return fenglish
 }
 
-function hasAlef(char) {
-	return [A_BA_KOLAH, ALEF].includes(char)
+function fenglishLetter(letter) {
+	return lettersMap[letter] || letter
 }
